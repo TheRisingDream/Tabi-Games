f6ae3a98577e2f2bd027a5d56d18f9077bf852aa
TheRisingDream <egor9kaza@mail.ru>
2025-05-08 19:22:23 +0300

    websocket install and add in project

---
+++ table/pages/chat.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { io } from "socket.io-client";
+
+const socket = io();
+const isConnected = ref('');
+const transport = ref("N/A");
+socket.on("hello", (data) => {
+       isConnected.value = data
+    });
+</script>
+
+<template>
+<div>
+  <p>Status: {{ isConnected }}</p>
+</div>
+</template>
